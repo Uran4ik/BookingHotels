be08c892097f4848d87fb246dfd7c14c1e3e2a55
Nurmuhammed <nur-zhar-2007@mail.ru>
2024-12-11 13:50:18 +0300

    Add UserPage

--- BlazorClient/obj/Debug/net8.0/BlazorClient.AssemblyInfo.cs
+++ BlazorClient/obj/Debug/net8.0/BlazorClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bca346e36f2f97c8c611a2b7b5928cd422d43d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18ceb04ef8b02935c49b4a38329a445cb421f8f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
